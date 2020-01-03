5f332f1a006dc7e9925b22d66e97865e084f3731
czoido <mrgalleta@gmail.com>
2020-01-03 10:36:03 +0100

    update python

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,17 +8,13 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew outdated pyenv || brew upgrade pyenv
     brew install pyenv-virtualenv
     brew install cmake || true
-    brew install openssl
 
     if which pyenv > /dev/null; then
         eval "$(pyenv init -)"
     fi
 
-    CFLAGS="-I$(brew --prefix openssl)/include" \
-    LDFLAGS="-L$(brew --prefix openssl)/lib" \
-
-    pyenv install 2.7.10
-    pyenv virtualenv 2.7.10 conan
+    pyenv install 3.7.1
+    pyenv virtualenv 3.7.1 conan
     pyenv rehash
     pyenv activate conan
 fi
