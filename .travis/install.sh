bbf01730aaf290390dde0e0ac36e1a5df27b8b4c
czoido <mrgalleta@gmail.com>
2020-01-03 09:31:30 +0100

    minor changes

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,7 +7,7 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update || brew update
     brew outdated pyenv || brew upgrade pyenv
     brew install pyenv-virtualenv
-    brew install cmake
+    brew install cmake || true
 
     if which pyenv > /dev/null; then
         eval "$(pyenv init -)"
