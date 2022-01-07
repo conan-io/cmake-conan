60e03b057a511d51372bc6bfe2f3d89da37bd7d6
LtdSauce <46743879+LtdSauce@users.noreply.github.com>
2022-01-07 23:58:10 +0100

    Add Conan as a possible program name in conan_check
    This commit extends the `find_program` to also look for `Conan` in addition to `conan`.
    The reason for this change is, that conan is named `Conan` when installed with winget.

--- conan.cmake
+++ conan.cmake
@@ -797,7 +797,7 @@ macro(conan_check)
         message(STATUS "Conan: checking conan executable")
     endif()
 
-    find_program(CONAN_CMD conan)
+    find_program(CONAN_CMD conan Conan)
     if(NOT CONAN_CMD AND CONAN_REQUIRED)
         message(FATAL_ERROR "Conan executable not found! Please install conan.")
     endif()
